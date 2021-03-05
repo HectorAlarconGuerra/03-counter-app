12fbc40b5cbc0ea356cd806763058a93b4767d12
Hector <hectoralarconguerra@gmail.com>
2021-03-05 16:06:39 -0500

    Pruebas2

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,5 +1,19 @@
+describe('Pruebas en el archivo demo.test.js',()=>{
 
-test('deben de ser iguales los string ', () => {
+
+    test('deben de ser iguales los strings', () => {
+  
+        // 1. inilización
+        const mensaje = 'Hola Mundo';
     
- 
-})
+        // 2. estímulo
+        const mensaje2 = `Hola Mundo`;
+    
+        // 3. Observar el comportamiento
+        expect (mensaje).toBe(mensaje2); // ===
+      
+    })
+    
+
+});
+
